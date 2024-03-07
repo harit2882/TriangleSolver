e48d59167412175c77cf5df08f422f71fdb5dbb8
Harit Thoriya <thoriyaharit@gmail.com>
2024-03-06 21:21:32 -0500

    Added equilateral triangle test case

--- TriangleSolverTesting/TriangleSolverUnitTesting.cs
+++ TriangleSolverTesting/TriangleSolverUnitTesting.cs
@@ -1,13 +1,32 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TriangleSolver;
 
 namespace TriangleSolverTesting
 {
-    public class Class1
+    public class TriangleSolverUnitTesting
     {
+        // Test Case For Equilateral Triangle Validation
+        [Test]
+        public void ValidEquilateralTriangle_Input60and60and60_OutputEquilateralTriangle()
+        {
+            // Arrange
+            int firstSide = 60;
+            int secondSide = 60;
+            int thirdSide = 60;
 
+            string expected = "Equilateral triangle";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
     }
 }
+
