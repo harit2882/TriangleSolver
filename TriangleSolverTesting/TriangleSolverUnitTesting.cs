50ddd670d534132ffc3b4a25ef0df7084d3e42ea
Harit Thoriya <thoriyaharit@gmail.com>
2024-03-06 21:23:03 -0500

    Added isosceles triangle test cases

--- TriangleSolverTesting/TriangleSolverUnitTesting.cs
+++ TriangleSolverTesting/TriangleSolverUnitTesting.cs
@@ -27,6 +27,61 @@ namespace TriangleSolverTesting
             //Assert
             Assert.That(actual, Is.EqualTo(expected));
         }
+
+        // Test Case For Isosceles Triangle Validation For 1st and 2nd Side
+        [Test]
+        public void ValidIsoscelesTriangle_Input60and60and70_OutputIsoscelesTriangle()
+        {
+            //Arrange
+            int firstSide = 60;
+            int secondSide = 60;
+            int thirdSide = 70;
+
+            string expected = "Isosceles triangle";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        // Test Case For Isosceles Triangle Validation For 1st and 3rd Side
+        [Test]
+        public void ValidIsoscelesTriangle_Input60and70and60_OutputIsoscelesTriangle()
+        {
+            //Arrange
+            int firstSide = 60;
+            int secondSide = 70;
+            int thirdSide = 60;
+
+            string expected = "Isosceles triangle";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        // Test Case For Isosceles Triangle Validation For 2nd and 3rd Side
+        [Test]
+        public void ValidIsoscelesTriangle_Input70and60and60_OutputIsoscelesTriangle()
+        {
+            //Arrange
+            int firstSide = 70;
+            int secondSide = 60;
+            int thirdSide = 60;
+
+            string expected = "Isosceles triangle";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
     }
 }
 
